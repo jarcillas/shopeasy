dc220fe3ecf1812c0defe19bc759f4a5b8c17b7e
Jesreal <jesrealarcillas@gmail.com>
2023-05-12 16:30:19 +0800

    display quantity on HomeScreen

--- constants/prices.js
+++ constants/prices.js
@@ -6,42 +6,42 @@ export const customDataSet = [
   },
   {
     id: "1",
-    title: "Milk (500mL)",
+    title: "Milk - 500mL",
     value: 40,
   },
   {
     id: "2",
-    title: "Coke (1L)",
+    title: "Coke - 1L",
     value: 80,
   },
   {
     id: "3",
-    title: "Jasmine Rice (1kg)",
+    title: "Jasmine Rice - 1kg",
     value: 46,
   },
   {
     id: "4",
-    title: "Well-milled Rice (1kg)",
+    title: "Well-milled Rice - 1kg",
     value: 40,
   },
   {
     id: "5",
-    title: "Black Rice (1kg)",
+    title: "Black Rice - 1kg",
     value: 60,
   },
   {
     id: "6",
-    title: "Water (1L)",
+    title: "Water - 1L",
     value: 20,
   },
   {
     id: "7",
-    title: "Water (500mL)",
+    title: "Water - 500mL",
     value: 14,
   },
   {
     id: "8",
-    title: "Soy sauce (250mL)",
+    title: "Soy sauce - 250mL",
     value: 16,
   },
 ];
